1be2c417b15903b3a85d776bf699b73e2961c3be
B. Mahrt <git@ursus.uber.space>
2022-12-04 00:47:12 +0100

    aoc22-3b: right in python

--- aoc22_3.py
+++ aoc22_3.py
@@ -1,10 +1,16 @@
 compart_a_list: dict = {}
 compart_b_list: dict = {}
+three_compart_a_list: dict = {}
+three_compart_b_list: dict = {}
+three_compart_c_list: dict = {}
 overlap_list: list = []
+three_overlap_list: list = []
 prio:int = 0
-
+three_prio:int = 0
+line_count:int = 0
 with open("input3","r") as file:
     for line in file:
+        line_count += 1
         line = line.strip()
         length_line = int(len(line)) // 2
         compart_a = line[:length_line]
@@ -14,25 +20,62 @@ with open("input3","r") as file:
             compart_a_list[letter] = compart_a_list.get(letter,0) + 1
         for letter in compart_b:
             compart_b_list[letter] = compart_b_list.get(letter,0) + 1
-        print(compart_a_list)
-        for entry in compart_a_list:
-            print(entry)
+        if line_count % 3 == 1:
+            for letter in line:
+                three_compart_a_list[letter] = three_compart_a_list.get(letter,0) + 1
+        elif line_count % 3 == 2:
+            for letter in line:
+                three_compart_b_list[letter] = three_compart_b_list.get(letter,0) + 1
+        elif line_count % 3 == 0:
+            for letter in line:
+                three_compart_c_list[letter] = three_compart_c_list.get(letter,0) + 1
+  
+#        print(compart_a_list)
+        print('3A: ' + str(three_compart_a_list))
+        print('3B: ' + str(three_compart_b_list))
+        print('3C: ' + str(three_compart_c_list))
+#        for entry in compart_a_list:
+#            print(entry)
+#            try:
+#                if compart_b_list[entry] != None:
+#                    overlap_list.append(entry)
+#            except:
+#                print(entry + ' not found in compart_b_list.')
+#        del compart_a_list
+#        compart_a_list: dict = {}
+#        del compart_b_list
+#        compart_b_list: dict = {}
+        for entry in three_compart_a_list:
             try:
-                if compart_b_list[entry] != None:
-                    overlap_list.append(entry)
+                if three_compart_b_list[entry] != None and three_compart_c_list[entry] != None:
+                    three_overlap_list.append(entry)
             except:
-                print(entry + ' not found in compart_b_list.')
-        del compart_a_list
-        compart_a_list: dict = {}
-        del compart_b_list
-        compart_b_list: dict = {}
-    print('overlap_list: ' + str(overlap_list))
-    for overlap_entry in overlap_list:
+                print(entry + ' not found in three_compart_b_list.')
+        if line_count % 3 == 0:
+            print('del lists')
+            del three_compart_a_list
+            three_compart_a_list : dict = {}
+            del three_compart_b_list
+            three_compart_b_list : dict = {}
+            del three_compart_c_list
+            three_compart_c_list : dict = {}
+    print('three_overlap_list: ' + str(three_overlap_list))
+
+#    for overlap_entry in overlap_list:
+#        if 65 <= ord(overlap_entry) <= 90:
+#            prio += ord(overlap_entry) - 64 + 26
+#            print('Letter ' + overlap_entry + ' with priority: ' + str(int(ord(overlap_entry)-64+26)))
+#        elif 97 <= ord(overlap_entry) <= 122:
+#            prio += ord(overlap_entry) - 96
+#            print('Letter ' + overlap_entry + ' with priority: ' + str(int(ord(overlap_entry)-96)))
+#    print('Prio: ' + str(prio))
+    for overlap_entry in three_overlap_list:
         if 65 <= ord(overlap_entry) <= 90:
-            prio += ord(overlap_entry) - 64 + 26
+            three_prio += ord(overlap_entry) - 64 + 26
             print('Letter ' + overlap_entry + ' with priority: ' + str(int(ord(overlap_entry)-64+26)))
         elif 97 <= ord(overlap_entry) <= 122:
-            prio += ord(overlap_entry) - 96
+            three_prio += ord(overlap_entry) - 96
             print('Letter ' + overlap_entry + ' with priority: ' + str(int(ord(overlap_entry)-96)))
-    print('Prio: ' + str(prio))
+    print('three_Prio: ' + str(three_prio)) # 9169 is to high
+
 
