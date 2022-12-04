27da602fcea917dcc63ed55e9337ffac61035833
B. Mahrt <git@ursus.uber.space>
2022-12-04 17:46:38 +0100

    aoc22-4: b right in python

--- aoc22_4.py
+++ aoc22_4.py
@@ -1,5 +1,5 @@
 cnt_overlap:int = 0
-
+cnt_any_overlap:int = 0
 with open('input4','r') as file:
     for line in file:
         part_a = line.split(',')[0]
@@ -17,10 +17,32 @@ with open('input4','r') as file:
            short_stop = int(part_b.split('-')[1])
            long_start = int(part_a.split('-')[0])
            long_stop = int(part_a.split('-')[1])
+
         print('short_start: ' + str(short_start) + ' short_stop: ' + str(short_stop) + ' long_start: ' + str(long_start) + ' long_stop: ' + str(long_stop))
         if short_start >= long_start and short_stop <= long_stop:
             cnt_overlap += 1
             print('overlap found for line: ' + str(line))
         else:
             print('no overlap for line: ' + str(line))
+        start_a = int(part_a.split('-')[0])
+        start_b = int(part_b.split('-')[0])
+        stop_a = int(part_a.split('-')[1])
+        stop_b = int(part_b.split('-')[1])
+        if start_a == start_b:
+            cnt_any_overlap += 1
+            continue
+        if stop_a == stop_b:
+            cnt_any_overlap += 1
+            continue
+        if start_a < start_b:
+            if start_b <= stop_a:
+                cnt_any_overlap += 1
+                continue
+        elif start_b < start_a:
+            if start_a <= stop_b:
+                cnt_any_overlap += 1
+                continue
     print('Overlap: ' + str(cnt_overlap))
+    print('Any overlap: ' + str(cnt_any_overlap))
+    
+
